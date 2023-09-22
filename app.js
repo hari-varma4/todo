f337c6977d454c1319f5efc825352fb9600e4113
hari-varma4 <appaniharikumar4@gmail.com>
2023-09-22 11:04:33 +0000

    testcases

--- app.js
+++ app.js
@@ -81,3 +81,65 @@ app.get("/todos/", async (request, response) => {
   data = await db.all(getTodosQuery);
   response.send(data);
 });
+
+app.get("/todos/:todoId/", async (request, response) => {
+  const { todoId } = request.params;
+  const quer = `
+    select * from todo where 
+    id=${todoId}
+    `;
+  const re = await db.get(quer);
+  response.send(re);
+  console.log(re);
+});
+app.post("/todos/", async (request, response) => {
+  const details = request.body;
+  const { id, todo, priority, status } = details;
+  const qu = `
+    insert into todo (id,todo,priority,status)
+    values(${id},"${todo}","${priority}","${status}")
+    `;
+  const re = await db.run(qu);
+  response.send("Todo Successfully Added");
+  console.log(re);
+});
+
+app.put("/todos/:todoId/", async (request, response) => {
+  const { todoId } = request.params;
+  const { status, priority, todo } = request.body;
+
+  const qu = null;
+  if (status !== undefined) {
+    const qu = `
+        update todo set id=${todoId}
+         where id=${todoId}
+        `;
+    const re = await db.run(qu);
+    response.send("Status Updated");
+  } else if (priority !== undefined) {
+    const qu = `
+        update todo set priority="${priority}"
+        where id=${todoId}
+       
+        `;
+    const re = await db.run(qu);
+    response.send("Priority Updated");
+  } else if (todo !== undefined) {
+    const qu = `
+        update todo set todo="${todo}"
+         where id=${todoId}
+        `;
+    const re = await db.run(qu);
+    response.send("Todo Updated");
+  }
+});
+
+app.delete("/todos/:todoId/", async (request, response) => {
+  const { todoId } = request.params;
+  const quu = `
+    delete from todo where id=${todoId}
+    `;
+  await db.run(quu);
+  response.send("Todo Deleted");
+});
+module.exports = app;
